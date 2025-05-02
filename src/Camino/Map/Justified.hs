fb2acd6d29aceff8c30295aa89570397733a89dd
exclusive-and <exclusiveandgate@gmail.com>
2025-05-02 16:04:19 -0400

    Moved $toplevel_examples closer to relevant functions

--- src/Camino/Map/Justified.hs
+++ src/Camino/Map/Justified.hs
@@ -74,48 +74,12 @@ lookup (Key k) (JustMap m) = case Map.lookup k m of
     Just v  -> v
     Nothing -> error "impossible: Camino.Map.Justified has been subverted!"
 
--- | Internal: coerce a function on 'Map's into one on 'JustMap's.
-
-coerceMap   :: (Map kx x -> Map ky y)
-            -> JustMap ph kx x
-            -> JustMap ph' ky y
-
-coerceMap f (JustMap m) = JustMap (f m)
-
--- | Internal: 'coerceMap' for functions with functorial results.
-
-coerceFmap  :: Functor f
-            => (Map kx x -> f (Map ky y))
-            -> JustMap ph kx x
-            -> f (JustMap ph' ky y)
-
-coerceFmap f (JustMap m) = JustMap <$> f m
-
--- | Internal: coerce a function into thinking that @ph@ remembers @k@.
-
-coerceKey :: (Key ph k -> r) -> k -> r
-coerceKey f = f . Key
-
--- | Map over the keys and values in a 'JustMap'.
-
-mapWithKey :: (Key ph k -> a -> b) -> JustMap ph k a -> JustMap ph k b
-mapWithKey f = coerceMap (Map.mapWithKey $ coerceKey f)
-
--- | Traverse over the keys and values in a 'JustMap'.
-
-traverseWithKey :: Applicative f
-                => (Key ph k -> a -> f b)
-                -> JustMap ph k a
-                -> f (JustMap ph k b)
-
-traverseWithKey f = coerceFmap (Map.traverseWithKey $ coerceKey f)
-
 {- $toplevel_examples
 
 == __Examples__
 
-Here's an example program that demonstrates how 'member' justifies 'lookup'
-operations within the appropriate scope at compile-time:
+Here's an example program that demonstrates how 'member' justifies 'lookup' operations
+within the appropriate scope at compile-time:
 
 @
     let sample = Map.fromList [(1, "hello"), (2, "world"), (3, "!")]
@@ -151,3 +115,39 @@ Output:
     hello world
 @
 -}
+
+-- | Internal: coerce a function on 'Map's into one on 'JustMap's.
+
+coerceMap   :: (Map kx x -> Map ky y)
+            -> JustMap ph kx x
+            -> JustMap ph' ky y
+
+coerceMap f (JustMap m) = JustMap (f m)
+
+-- | Internal: 'coerceMap' for functions with functorial results.
+
+coerceFmap  :: Functor f
+            => (Map kx x -> f (Map ky y))
+            -> JustMap ph kx x
+            -> f (JustMap ph' ky y)
+
+coerceFmap f (JustMap m) = JustMap <$> f m
+
+-- | Internal: coerce a function into thinking that @ph@ remembers @k@.
+
+coerceKey :: (Key ph k -> r) -> k -> r
+coerceKey f = f . Key
+
+-- | Map over the keys and values in a 'JustMap'.
+
+mapWithKey :: (Key ph k -> a -> b) -> JustMap ph k a -> JustMap ph k b
+mapWithKey f = coerceMap (Map.mapWithKey $ coerceKey f)
+
+-- | Traverse over the keys and values in a 'JustMap'.
+
+traverseWithKey :: Applicative f
+                => (Key ph k -> a -> f b)
+                -> JustMap ph k a
+                -> f (JustMap ph k b)
+
+traverseWithKey f = coerceFmap (Map.traverseWithKey $ coerceKey f)
